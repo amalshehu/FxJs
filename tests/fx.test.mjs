e46440e8e1ca79369177fbd6f6f903c4ef52e640
amalshehu <amalshehu@gmail.com>
2018-06-06 09:12:34 +0530

    Modify tests

--- tests/fx.test.mjs
+++ tests/fx.test.mjs
@@ -1,23 +1,6 @@
 import Fx from '../main.mjs'
-
-class Square extends Fx.FxBuilder {
-  onPerceive(value) {
-    this.send(value ** 2)
-  }
-}
-
-class Write extends Fx.FxBuilder {
-  onPerceive(value) {
-    // test(`Take down-stream node as arg then square and pipe: ${value} `, () => {
-    //   expect(value).toBeTruthy()
-    // })
-    console.log(value)
-  }
-}
-
-const square = new Square()
-const write = new Write()
+import '../operators.mjs'
 
 Fx.array([1, 2, 3])
-  .tube(square)
-  .tube(write)
+  .times(12)
+  .print()
